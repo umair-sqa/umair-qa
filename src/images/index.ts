a0f70bdff14cc7d52655ebe9411cd2664b76c349
Umair Haider <cheemaumair912@gmail.com>
2025-10-18 20:18:31 +0500

    old portfolio

--- src/images/index.ts
+++
@@ -1,4 +0,0 @@
-export {default as heroImage} from './header-background.jpg';
-export * from './portfolio';
-export {default as profilepic} from './profilepic.jpg';
-export {default as testimonialImage} from './testimonial.jpg';
